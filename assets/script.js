cd9445732570d70125630346e5edda8d6dda7707
coderarifbd <coder.arifbd@gmail.com>
2024-12-09 20:20:59 +0600

    done

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
 console.log("alert");
+
+const sitName = document.getElementsByClassName("kbd");
+let sitNumber = "";
+
+for (let i = 0; i < sitName.length; i++) {
+  sitName[i].addEventListener("click", function () {
+    sitNumber = sitName[i].innerText;
+    // sitName[i].classList.toggle("bg-green");
+    sitConfimation(sitNumber);
+  });
+}
+
+console.log(sitName);
+
+function sitConfimation(sit) {
+  sit.classList.toggle("bg-green");
+  const tableRow = `<tr>
+                        <td>${sit}</td>
+                        <td>Economoy</td>
+                        <td>550</td>
+                    </tr>`;
+  //   console.log(tableRow);
+  const tBody = document.getElementById("sit-confirm");
+  console.log(tBody);
+  tBody.innerHTML += tableRow;
+}
