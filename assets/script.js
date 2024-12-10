d073d2f2eaab8da270933208f53f7845637d3cef
coderarifbd <coder.arifbd@gmail.com>
2024-12-10 08:03:24 +0600

    total calculation doen

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,39 @@
-console.log("alert");
+// console.log("alert");
 
 const sitName = document.getElementsByClassName("kbd");
 let sitNumber = "";
 
 for (let i = 0; i < sitName.length; i++) {
   sitName[i].addEventListener("click", function () {
-    sitNumber = sitName[i].innerText;
     // sitName[i].classList.toggle("bg-green");
-    sitConfimation(sitNumber);
+    sitConfimation(sitName[i]);
   });
 }
 
-console.log(sitName);
-
 function sitConfimation(sit) {
-  sit.classList.toggle("bg-green");
-  const tableRow = `<tr>
-                        <td>${sit}</td>
-                        <td>Economoy</td>
-                        <td>550</td>
-                    </tr>`;
-  //   console.log(tableRow);
-  const tBody = document.getElementById("sit-confirm");
-  console.log(tBody);
-  tBody.innerHTML += tableRow;
+  const sitId = sit.innerHTML;
+  const existingSitId = document.getElementById(sitId);
+  if (!existingSitId) {
+    const sitConfimation = document.getElementById("sit-confirm");
+    console.log();
+    if (sitConfimation.children.length < 4) {
+      sit.classList.toggle("bg-green");
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<td id="${sitId}">${sitId}</td>
+                  <td>Economoy</td>
+                  <td>550</td>`;
+      const tBody = document.getElementById("sit-confirm");
+      tBody.appendChild(tr);
+      totalCalculation(sitConfimation.children.length);
+    } else {
+      alert("You can select maximum 4 sit!");
+    }
+  } else {
+    existingSitId.parentNode.remove();
+  }
+}
+
+function totalCalculation(s) {
+  const total = document.getElementById("total");
+  total.innerText = 550 * s;
 }
